d956fb937366041d22ab98d5118194e284c447a1
nwaynwayzayya <6631503081@lamduan.mfu.ac.th>
2025-07-16 21:30:51 +0700

    Add all functions and stylings

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Display controller to show message like who wins and if it's a tie
+const displayController = (() => {
+    const renderMessage = (message) => {
+        document.querySelector("#message").innerHTML = message;
+    }
+
+    return {
+        renderMessage,
+    }
+})();
+
+
+
 // Gameboard module
 const Gameboard = (() => {
     let gameboard = ["", "", "", "", "", "", "", "", ""];
@@ -67,6 +80,9 @@ const Game = (() => {
     }
 
     const handleClick = (event) => {
+        if (gameOver) {
+            return;
+        }
         let index = parseInt(event.target.id.split("-")[1]);
 
         // Makes sure the player can't click the square twice
@@ -75,8 +91,19 @@ const Game = (() => {
         }
 
         Gameboard.update(index, players[currentPlayerIndex].mark);
+
+        if (checkForWin(Gameboard.getGameboard(), players[currentPlayerIndex].mark)) {
+            gameOver = true; 
+            displayController.renderMessage(`${players[currentPlayerIndex].name} won!`);
+        } else if  (checkForTie(Gameboard.getGameboard())) {
+            gameOver = true;
+            displayController.renderMessage(`It's a tie!`);
+        }
+
         // logic to switch players
         currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+
+
     }
 
     // Function to restart the game
@@ -85,6 +112,8 @@ const Game = (() => {
             Gameboard.update(i, "");
         }
         Gameboard.render();
+        gameOver = false;
+        document.querySelector("#message").innerHTML = "";
     }
 
     return {
@@ -94,6 +123,33 @@ const Game = (() => {
     }
 })();
 
+
+/// Function for checking for win
+function checkForWin(board) {
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5], 
+        [6, 7, 8], 
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8], 
+        [0, 4, 8], 
+        [2, 4, 6]
+    ]  
+    for (let i = 0; i < winningCombinations.length; i++) {
+        const [a, b, c] = winningCombinations[i];
+
+        if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Function to check for ties
+function checkForTie(board) {
+    return board.every(cell => cell !== "");
+}
 
 // Restart button variable and event listener for it
 const restartButton = document.querySelector("#restart-button");
