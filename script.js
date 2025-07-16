123fc56a20dafd158dc3de247c2d8589db128e1c
nwaynwayzayya <6631503081@lamduan.mfu.ac.th>
2025-07-16 13:06:18 +0700

    Add functions for the gameboard to play

--- script.js
+++ script.js
@@ -17,8 +17,20 @@ const Gameboard = (() => {
             square.addEventListener("click", Game.handleClick);
         })
     }
+    // Method to update the square with X or O
+    const update = (index, value) => {
+        gameboard[index] = value;
+        render();
+    }
+
+    // Accessor method to gameboard
+    // This returns the gameboard indirectly so we cannot modify it
+    const getGameboard = () => gameboard;
+
     return {
         render, 
+        update, 
+        getGameboard,
     }
 })();
 
@@ -45,20 +57,51 @@ const Game = (() => {
         currentPlayerIndex = 0;
         gameOver = false;
         Gameboard.render();
+        // select all the squares on the gameboard
+        const squares = document.querySelectorAll(".square");
+        // function to click the squares
+        // handleClick is called from the module Game
+        squares.forEach((square) => {
+            square.addEventListener("click", handleClick);
+        })
     }
 
     const handleClick = (event) => {
         let index = parseInt(event.target.id.split("-")[1]);
-        console.log(index);
+
+        // Makes sure the player can't click the square twice
+        if (Gameboard.getGameboard() [index] !== "") {
+            return;
+        }
+
+        Gameboard.update(index, players[currentPlayerIndex].mark);
+        // logic to switch players
+        currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+    }
+
+    // Function to restart the game
+    const restart = () => {
+        for (let i = 0; i < 9; i++) {
+            Gameboard.update(i, "");
+        }
+        Gameboard.render();
     }
 
     return {
         start,
         handleClick,
+        restart,
     }
 })();
 
-// Start button variable and event listener for it.
+
+// Restart button variable and event listener for it
+const restartButton = document.querySelector("#restart-button");
+restartButton.addEventListener("click", () => {
+    Game.restart();
+})
+
+// Start button variable and event listener for it
 const startButton = document.querySelector("#start-button");
 startButton.addEventListener("click", () => {
     Game.start();
